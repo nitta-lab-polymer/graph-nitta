644c102b00f51aa3adbfc3c532f33f5f2b700df6
Mkamono <chengyaye31@gmail.com>
2024-02-06 04:21:14 +0000

    グラフの間隔を設定

--- src/graph_nitta/graph.py
+++ src/graph_nitta/graph.py
@@ -13,13 +13,22 @@ def calc_grid(number_of_subplots: int) -> int:
 
 
 def make_graph(
-    number_of_subplots: int = 1, row: Optional[int] = None, column: Optional[int] = None
+    number_of_subplots: int = 1,
+    row: Optional[int] = None,
+    column: Optional[int] = None,
+    wspace: float = 0.7,
+    hspace: float = 0.3,
 ) -> Tuple[Figure, List[Axes]]:
     apply_basic_style()
     fig = plt.figure()
     grid = calc_grid(number_of_subplots)
     row = row or grid
     column = column or grid
-    fig.set_size_inches(8 * column, 8 * row)
+
+    width = 8 * column + (wspace * 10) * (column - 1)
+    hight = 8 * row + (hspace * 10) * (row - 1)
+
+    fig.set_size_inches(width, hight)
+    fig.subplots_adjust(wspace=wspace, hspace=hspace)
     axes = [fig.add_subplot(row, column, i + 1) for i in range(number_of_subplots)]
     return fig, axes
