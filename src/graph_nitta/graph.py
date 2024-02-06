f6ba4c3cacd60cf4659dfdb7519cc5da208116ac
Mkamono <chengyaye31@gmail.com>
2024-02-06 04:40:26 +0000

    グラフにアルファベットを追加する機能を実装

--- src/graph_nitta/graph.py
+++ src/graph_nitta/graph.py
@@ -12,12 +12,26 @@ def calc_grid(number_of_subplots: int) -> int:
     return math.ceil(math.sqrt(number_of_subplots))
 
 
+def add_alphabet_to_axes(axes: List[Axes]) -> None:
+    for i, ax in enumerate(axes):
+        ax.text(
+            x=0.07,
+            y=0.94,
+            ha="center",
+            va="center",
+            s=f"({chr(97 + i)})",
+            fontsize=30,
+            transform=ax.transAxes,
+        )
+
+
 def make_graph(
     number_of_subplots: int = 1,
     row: Optional[int] = None,
     column: Optional[int] = None,
     wspace: float = 0.7,
     hspace: float = 0.3,
+    is_visible_id: bool = True,
 ) -> Tuple[Figure, List[Axes]]:
     apply_basic_style()
     fig = plt.figure()
@@ -31,4 +45,7 @@ def make_graph(
     fig.set_size_inches(width, hight)
     fig.subplots_adjust(wspace=wspace, hspace=hspace)
     axes = [fig.add_subplot(row, column, i + 1) for i in range(number_of_subplots)]
+
+    if number_of_subplots > 1 and is_visible_id:
+        add_alphabet_to_axes(axes)
     return fig, axes
