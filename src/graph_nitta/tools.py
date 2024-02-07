ece6f49f43229e395fe2f05436ad739fe48cad57
Mkamono <chengyaye31@gmail.com>
2024-02-07 17:03:41 +0000

    tools.pyの変更: 色の線形補間関数を追加

--- src/graph_nitta/tools.py
+++ src/graph_nitta/tools.py
@@ -28,3 +28,32 @@ def arrow(
         verticalalignment="bottom",
         transform=ax.transAxes,
     )
+
+
+def get_colors(
+    start: tuple[float, float, float, float],
+    end: tuple[float, float, float, float],
+    number: int,
+) -> list[tuple[float, float, float, float]]:
+    """色を線形補間で生成する
+
+    Args:
+        start (tuple[float, float, float, float]): 最初の色
+        end (tuple[float, float, float, float]): 最後の色
+        number (int): 分割数
+
+    Returns:
+        list[tuple[float, float, float, float]]: 色のリスト。RGBAの順
+    """
+
+    maps = []
+    for i in range(number):
+        maps.append(
+            (
+                start[0] + (end[0] - start[0]) * i / (number - 1),
+                start[1] + (end[1] - start[1]) * i / (number - 1),
+                start[2] + (end[2] - start[2]) * i / (number - 1),
+                start[3] + (end[3] - start[3]) * i / (number - 1),
+            )
+        )
+    return maps
